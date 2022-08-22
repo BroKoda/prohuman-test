2c1668c49d75dfd2a5e1a57d9899cb258463b187
Konrad Kavai <kottix96@gmail.com>
2022-08-22 02:17:49 +0200

    prim szamok vizsgalata a postcode-ban

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { RandomPerson } from './model/randomPeople';
 export class AppComponent implements OnInit {
   title = 'prohuman-test';
   randomPeople: RandomPerson[] = []
-  gyoztunk: object;
+  testRandomPerson: any[];
 
   constructor(private http: HttpClient) {}
 
@@ -20,7 +20,9 @@ export class AppComponent implements OnInit {
   }
 
   private getRandomPeople() {
-    this.http.get<{[key: string]: RandomPerson}>('https://randomuser.me/api/?results=50')
+    // Csak azert van kommentben az URL-es hivas, mert 10-bol 8 alkalommal nem tolt be :)
+    // this.http.get<{[key: string]: RandomPerson}>('https://randomuser.me/api/?results=500')
+    this.http.get<{[key: string]: RandomPerson}>('../assets/randomPeople.json')
       .pipe(map((res) => {
         const randomPeople = [];
         for (const key in res) {
@@ -32,7 +34,48 @@ export class AppComponent implements OnInit {
       }))
       .subscribe((randomPeople) => {
         this.randomPeople = randomPeople;
-        console.log(this.randomPeople)
     })
+  }
+
+  checkForPrimeNumbersInPostcode(postcode) {
+    let numberString = postcode.toString();
+    let numbersOnly = [];
+    let primeNumbers = 0;
+
+    // POSTCODE betuk kizarasa az egyenletbol
+    for (let i = 0; i < numberString.length; i++) {
+      if (!Number.isNaN(parseInt(numberString.charAt(i)))) {
+        numbersOnly.push(parseInt(numberString.charAt(i)));
+      }
+    }
+
+    // Mennyi prim szam talalhato a szamok kozott?
+    for (let i = 0; i < numbersOnly.length; i++) {
+
+      // A nulla nem prim szam
+      if (numbersOnly[i] !== 0 ){
+        let primeNumberHelper = 0
+
+        // j nem 1-tol indul, mert minden szam oszthato 1-el
+        // ha van osztoja a szamnak, akkor az a szam felenel mindig kisebb
+        for (let j = 2; j <= numbersOnly[i]/2; j++) {
+          if (numbersOnly[i] % j === 0) {
+            primeNumberHelper += 1;
+          }
+        }
+
+        // Ha a Helper 0 akkor csak magaval oszthato a szam
+        if (primeNumberHelper === 0) {
+          primeNumbers += 1;
+        }
+      }
+    }
+
+    // ha legalabb 2 prim szam van akkor megjelenik a template
+    if (primeNumbers > 1) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
